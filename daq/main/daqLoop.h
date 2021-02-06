0dd41bc022b63319f5b4b3d0bcb0ca45a7a11746
James Ferguson <phooobs@gmail.com>
2021-02-05 22:11:11 -0700

    setup simple log data to file

--- daq/main/daqLoop.h
+++ daq/main/daqLoop.h
@@ -18,45 +18,36 @@
 
 #include "daqPacket.h"
 
+// SD card connections
+    // GPIO 15 -- CMD -- 10k pullup
+    // GPIO 14 -- CLK
+    // GPIO 2 -- GPIO 0 -- D0 -- 10k pullup
+    // D3 -- 10k pullup
+
 void daqLoop (void * pvParameters) {
     struct DAQPacket daqPacket;
 
     // 1-line SD card init
-    static const char *TAG = "example";
+    static const char *TAG = "SD card";
     ESP_LOGI(TAG, "Using SDMMC peripheral");
     sdmmc_host_t host = SDMMC_HOST_DEFAULT();
-
-    // This initializes the slot without card detect (CD) and write protect (WP) signals.
-    // Modify slot_config.gpio_cd and slot_config.gpio_wp if your board has these signals.
     sdmmc_slot_config_t slot_config = SDMMC_SLOT_CONFIG_DEFAULT();
-
-    // To use 1-line SD mode, uncomment the following line:
     slot_config.width = 1;
 
-    // GPIOs 15, 2, 4, 12, 13 should have external 10k pull-ups.
-    // Internal pull-ups are not sufficient. However, enabling internal pull-ups
-    // does make a difference some boards, so we do that here.
-    gpio_set_pull_mode(15, GPIO_PULLUP_ONLY);   // CMD, needed in 4- and 1- line modes
-    gpio_set_pull_mode(2, GPIO_PULLUP_ONLY);    // D0, needed in 4- and 1-line modes
-    // gpio_set_pull_mode(4, GPIO_PULLUP_ONLY);    // D1, needed in 4-line mode only
-    // gpio_set_pull_mode(12, GPIO_PULLUP_ONLY);   // D2, needed in 4-line mode only
-    gpio_set_pull_mode(13, GPIO_PULLUP_ONLY);   // D3, needed in 4- and 1-line modes
+    // Set internal GPIO pullups for SD card pins
+    gpio_set_pull_mode(GPIO_NUM_15, GPIO_PULLUP_ONLY); // CMD
+    gpio_set_pull_mode(GPIO_NUM_2, GPIO_PULLUP_ONLY); // D0
 
     // Options for mounting the filesystem.
-    // If format_if_mount_failed is set to true, SD card will be partitioned and
-    // formatted in case when mounting fails.
     esp_vfs_fat_sdmmc_mount_config_t mount_config = {
         .format_if_mount_failed = false,
         .max_files = 5,
         .allocation_unit_size = 16 * 1024
     };
 
-    // Use settings defined above to initialize SD card and mount FAT filesystem.
-    // Note: esp_vfs_fat_sdmmc_mount is an all-in-one convenience function.
-    // Please check its source code and implement error recovery when developing
-    // production applications.
+    // Try to mount SD card at /sd
     sdmmc_card_t* card;
-    esp_err_t ret = esp_vfs_fat_sdmmc_mount("/sdcard", &host, &slot_config, &mount_config, &card);
+    esp_err_t ret = esp_vfs_fat_sdmmc_mount("/sd", &host, &slot_config, &mount_config, &card);
 
     if (ret != ESP_OK) {
         if (ret == ESP_FAIL) {
@@ -66,59 +57,8 @@ void daqLoop (void * pvParameters) {
             ESP_LOGE(TAG, "Failed to initialize the card (%s). "
                 "Make sure SD card lines have pull-up resistors in place.", esp_err_to_name(ret));
         }
-        return;
+        vTaskDelete(NULL); // kill this task
     }
-
-    // Card has been initialized, print its properties
-    sdmmc_card_print_info(stdout, card);
-
-    // Use POSIX and C standard library functions to work with files.
-    // First create a file.
-    ESP_LOGI(TAG, "Opening file");
-    FILE* f = fopen("/sdcard/hello.txt", "w");
-    if (f == NULL) {
-        ESP_LOGE(TAG, "Failed to open file for writing");
-        return;
-    }
-    fprintf(f, "Hello %s!\n", card->cid.name);
-    fclose(f);
-    ESP_LOGI(TAG, "File written");
-
-    // Check if destination file exists before renaming
-    struct stat st;
-    if (stat("/sdcard/foo.txt", &st) == 0) {
-        // Delete it if it exists
-        unlink("/sdcard/foo.txt");
-    }
-
-    // Rename original file
-    ESP_LOGI(TAG, "Renaming file");
-    if (rename("/sdcard/hello.txt", "/sdcard/foo.txt") != 0) {
-        ESP_LOGE(TAG, "Rename failed");
-        return;
-    }
-
-    // Open renamed file for reading
-    ESP_LOGI(TAG, "Reading file");
-    f = fopen("/sdcard/foo.txt", "r");
-    if (f == NULL) {
-        ESP_LOGE(TAG, "Failed to open file for reading");
-        return;
-    }
-    char line[64];
-    fgets(line, sizeof(line), f);
-    fclose(f);
-    // strip newline
-    char* pos = strchr(line, '\n');
-    if (pos) {
-        *pos = '\0';
-    }
-    ESP_LOGI(TAG, "Read from file: '%s'", line);
-
-    // All done, unmount partition and disable SDMMC or SPI peripheral
-    esp_vfs_fat_sdmmc_unmount();
-    ESP_LOGI(TAG, "Card unmounted");
-
 
     daqPacket.testValue = 0;
     for (;;) { // loop  forever
@@ -127,6 +67,23 @@ void daqLoop (void * pvParameters) {
         daqPacket.testValue++;
         vTaskDelay(10);
 
+        // opening and closing the file like this may be slow
+
+        // write data to SC card
+        // Create and open file
+        // note there is a file name length limit
+        FILE* sd_card_file = fopen("/sd/w_t.raw", "a");
+        if (sd_card_file == NULL) {
+            ESP_LOGE(TAG, "Failed to open file for writing");
+            vTaskDelete(NULL); // kill this task
+        }
+        fprintf(sd_card_file, "%i\n", daqPacket.testValue);            
+        fclose(sd_card_file);
+
         *(struct DAQPacket*)pvParameters = daqPacket; // send data in packet back to main
     }
+    
+    // Unmount SD card
+    esp_vfs_fat_sdmmc_unmount();
+    ESP_LOGI(TAG, "Card unmounted");
 }
