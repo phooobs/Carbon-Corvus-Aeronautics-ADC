81aa5b96cbba04f94cea354ab36ccb593e6db7e5
James Ferguson <phooobs@gmail.com>
2021-02-26 18:50:11 -0700

    moved good news code into the daq loop

--- arduino/daq/daqPacket.h
+++ arduino/daq/daqPacket.h
@@ -7,4 +7,5 @@
 struct DAQPacket {
   int testValue;
   uint8_t ms4525doData[4] = {0, 0, 0, 0}; // data recived from the airspeed sentor
+  uint8_t mpu9250Data[3][18]; // data recived from the accelerometer
 };
