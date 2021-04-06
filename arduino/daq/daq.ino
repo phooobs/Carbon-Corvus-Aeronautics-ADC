379e57ab22c0da42fab5e8b1126b8dfce8d43bae
James Ferguson <phooobs@gmail.com>
2021-04-06 17:30:18 -0600

    collected bias test data and made the system auto start data collection

--- arduino/daq/daq.ino
+++ arduino/daq/daq.ino
@@ -17,7 +17,7 @@ DAQPacket daqPacket;
 
 void delayDaqLoopStart(void *pvParameters) {
   pinMode(LED_PIN, OUTPUT);
-  for (int i = 0; i < 10; i++) {
+  for (int i = 0; i < 60; i++) {
     vTaskDelay(500 / portTICK_PERIOD_MS);
     digitalWrite(LED_PIN, HIGH);
     vTaskDelay(500 / portTICK_PERIOD_MS);
@@ -38,25 +38,22 @@ void setup() {
   // use uxTaskGetStackHighWaterMark() to figure out how big the stack sizes should actualy be
   ESP_LOGI("app_main", "Launching system task on core 0");
   xTaskCreatePinnedToCore(systemLoop, "System Task", 10000, (void*)&daqPacket, 1, NULL, 0);
-  
+  #ifdef DELAY_DAQ
+    ESP_LOGI("app_main", "Launching delay DAQ task on core 1");
+    xTaskCreatePinnedToCore(delayDaqLoopStart, "DAQ Task", 10000, (void*)&daqPacket, 10, NULL, 0);
+  #endif
+  #ifndef DELAY_DAQ
+    ESP_LOGI("app_main", "Launching DAQ task on core 1");
+    xTaskCreatePinnedToCore(daqLoop, "DAQ Task", 10000, (void*)&daqPacket, 10, NULL, 1);
+    ESP_LOGI("app_main", "Launching kill DAQ in 10 task on core 0");
+    xTaskCreatePinnedToCore(killDaqLoopDelay, "DAQ Task", 10000, NULL, 2, NULL, 0);
+  #endif  
 }
 
 void loop() { // get switch input for starting daq
   vTaskDelay(1000 / portTICK_PERIOD_MS);
   static bool last;
   bool now = digitalRead(SW_PIN);
-  if (now & !last) { // rising edge turn on
-    #ifdef DELAY_DAQ
-      ESP_LOGI("app_main", "Launching delay DAQ task on core 1");
-      xTaskCreatePinnedToCore(delayDaqLoopStart, "DAQ Task", 10000, (void*)&daqPacket, 10, NULL, 0);
-    #endif
-    #ifndef DELAY_DAQ
-      ESP_LOGI("app_main", "Launching DAQ task on core 1");
-      xTaskCreatePinnedToCore(daqLoop, "DAQ Task", 10000, (void*)&daqPacket, 10, NULL, 1);
-      ESP_LOGI("app_main", "Launching kill DAQ in 10 task on core 0");
-      xTaskCreatePinnedToCore(killDaqLoopDelay, "DAQ Task", 10000, NULL, 2, NULL, 0);
-    #endif
-  }
   if (!now & last) { // falling edge turn off
     ESP_LOGI("app_main", "killing DAQ loop");
     xTaskCreatePinnedToCore(killDaqLoop, "DAQ Task", 10000, NULL, 2, NULL, 0);
